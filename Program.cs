a362ded05db3c1e216400fccc72037902f6437bf
Olga <tov_84@inbox.ru>
2024-01-25 15:39:23 +0300

    Написан код программы для решения задачи

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿string[] array = new string[5];
+string[] arr = new string[array.Length];
+int k = 0;
+
+void CreateArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine()!;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void SelectItems()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arr[k] = array[i];
+            k++;
+        }
+    }
+}
+
+Console.WriteLine("Введите любые строки через пробел:");
+CreateArray();
+Console.Write("Исходный массив строк: ");
+PrintArray(array);
+SelectItems();
+if (k != 0)
+{
+    Console.Write("Полученный массив: ");
+    PrintArray(arr);
+}
+else Console.WriteLine("В заданном массиве нет срок с количеством символов меньше или равно 3");
